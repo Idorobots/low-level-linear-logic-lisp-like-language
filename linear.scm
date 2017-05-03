803f1b792ba116881a4b8be682c70755bc19a61a
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2017-05-03 13:55:50 +0200

    Added a stub of the main linear lisp components.

--- linear.scm
+++ linear.scm
@@ -1,1 +1,77 @@
-;; Linear-typed Lisp
+;;; Linear-typed Lisp
+
+;; Utils:
+
+(define (trace state)
+  (display state)
+  (newline))
+
+;; State: (C R1 R2 SP FR)
+
+(define c 0)
+(define r1 1)
+(define r2 2)
+(define sp 3)
+(define fr 4)
+
+(define (state c r1 r2 sp fr)
+  (list c r1 r2 sp fr))
+
+(define (reg state r)
+  (list-ref state r))
+
+(define (reg-set state r value)
+  ???)
+
+;; Opcodes:
+
+(define (op-null? r)
+  (lambda (state)
+    (if (equal? (reg state r) 'nil)
+        (reg-set state c 'true)
+        (reg-set state c 'nil))))
+
+(define (op-atom? r)
+  (lambda (state)
+    (if (symbol? (reg state r))
+        (reg-set state c 'true)
+        (reg-set state c 'nil))))
+
+(define (op-eq? r1 r2)
+  (lambda (state)
+    (if (eq? (reg state r1) (reg state r2))
+        (reg-set state c 'true)
+        (reg-set state c 'nil))))
+
+(define (op-swap r1 r2)
+  ???)
+
+(define (op-swap-car r1 r2)
+  ???)
+
+(define (op-swap-cdr r1 r2)
+  ???)
+
+(define (op-set r1 atom)
+  ???)
+
+(define (op-assign r1 r2)
+  ???)
+
+(define (op-cons r1 r2)
+  ???)
+
+(define (op-push r1 r2)
+  (op-cons r1 r2))
+
+(define (op-pop r1 r2)
+  ???)
+
+;; Run:
+
+(define (run state opcodes)
+  (foldl (lambda (s o)
+           (trace s)
+           (o s))
+         state
+         opcodes))
