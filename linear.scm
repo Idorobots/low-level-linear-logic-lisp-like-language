1827b5128af95d55484a64a464233526a18c271f
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2017-05-03 14:45:31 +0200

    Implemented op-set & op-assign.

--- linear.scm
+++ linear.scm
@@ -12,6 +12,8 @@
   (display state)
   (newline))
 
+(define atom? symbol?)
+
 ;; State: (C R1 R2 SP FR)
 
 (define c 0)
@@ -42,7 +44,7 @@
 
 (define (op-atom? r)
   (lambda (state)
-    (if (symbol? (reg state r))
+    (if (atom? (reg state r))
         (reg-set state c 'true)
         (reg-set state c 'nil))))
 
@@ -53,40 +55,49 @@
         (reg-set state c 'nil))))
 
 (define (op-set r1 atom)
-  ???)
+  (lambda (state)
+    (if (and (atom? (reg state r1))
+             (atom? atom))
+        (reg-set state r1 atom)
+        (reg-set state c 'op-set-error))))
 
 (define (op-assign r1 r2)
-  ???)
+  (lambda (state)
+    (let ((a (reg state r2)))
+      (if (and (atom? (reg state r1))
+               (atom? a))
+          (reg-set state r1 a)
+          (reg-set state c 'op-assign-error)))))
 
 (define (op-swap r1 r2)
   (lambda (state)
     (let ((a (reg state r1))
           (b (reg state r2)))
       (--> state
-          (reg-set r1 b)
-          (reg-set r2 a)))))
+           (reg-set r1 b)
+           (reg-set r2 a)))))
 
 (define (op-swap-car r1 r2)
   (lambda (state)
     (let ((a (reg state r1))
           (b (reg state r2)))
       (if (and (not (equal? r1 r2))
-               (not (symbol? b)))
+               (not (atom? b)))
           (--> state
                (reg-set r1 (car b))
                (reg-set r2 (cons a (cdr b))))
-          (reg-set state c 'error)))))
+          (reg-set state c 'op-swap-car-error)))))
 
 (define (op-swap-cdr r1 r2)
   (lambda (state)
     (let ((a (reg state r1))
           (b (reg state r2)))
       (if (and (not (equal? r1 r2))
-               (not (symbol? b)))
+               (not (atom? b)))
           (--> state
                (reg-set r1 (cdr b))
                (reg-set r2 (cons (car b) a)))
-          (reg-set state c 'error)))))
+          (reg-set state c 'op-swap-cdr-error)))))
 
 (define (op-cons r1 r2)
   ???)
@@ -111,12 +122,13 @@
 (trace
  (run
   (state 'nil 'nil 'nil 'nil '(nil nil nil))
-  (list (op-null? c)
-        (op-swap c r1)
-        (op-atom? r1)
+  (list (op-set r2 'true)
         (op-null? r1)
-        (op-eq? c r1)
-        (op-null? c)
+        (op-swap c r1)
+        (op-eq? r1 r2)
+        (op-atom? r2)
+        (op-set sp 'hello)
+        (op-assign c sp)
         (op-swap-car c fr)
         (op-null? c)
-        (op-swap-cdr c fr))))
+        (op-swap-cdr sp fr))))
