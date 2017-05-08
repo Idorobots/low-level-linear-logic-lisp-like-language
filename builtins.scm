c140646e6f4fc65d3c5a4f70b355a78ca942f6e8
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2017-05-08 22:50:02 +0200

    Converted fn-equal? to use the new calling convention.

--- builtins.scm
+++ builtins.scm
@@ -50,42 +50,39 @@
                                      (mc-push r2 t2)))
                     (op-set c 'fn-copy-error))))
 
-(define (fn-equal? r1 r2 r3)
-  (lambda (labels)
-    (fix-state
-     (lambda (state)
-       (run 'fn-equal? state
-            (mc-spill (list t1)
-                      ;; Check the condition.
-                      (mc-if (mc-and t1
-                                     (op-atom? r1)
-                                     (op-atom? r2))
-                             ;; Both atoms.
-                             (list
-                              (op-eq? r1 r2)
-                              (op-swap c r3))
-                             (mc-if (mc-and t1
-                                            (op-not t1)
-                                            (mc-not (op-atom? r2)))
-                                    ;; Both non-atoms.
-                                    (mc-spill (list t2 t3)
-                                              ;; Compute (car r1) & (car r2).
-                                              (mc-pop t1 r1)
-                                              (mc-pop t2 r2)
-                                              (fn-equal? r1 r2 r3)
-                                              (op-swap t1 r1)
-                                              (op-swap t2 r2)
-                                              (op-swap t3 r3) ;; Result of (equal? (cdr r1) (cdr r2)) is in t3.
-                                              (fn-equal? r1 r2 r3) ;; Result of (equal? (car r1) (car r2)) is in r3.
-                                              (op-and t3 r3)
-                                              (op-swap c r3) ;; Result of (and t3 r3) lands in r3.
-                                              ;; Restore arguments.
-                                              (op-swap t1 r1)
-                                              (op-swap t2 r2)
-                                              (mc-push r1 t1)
-                                              (mc-push r2 t2))
-                                    ;; An atom & non-atom.
-                                    (op-set r3 'nil)))))))))
+(define (fn-equal?)
+  (mc-define ':fn-equal? ; (r1 r2) -> r3
+             (mc-spill (list t1)
+                       ;; Check the condition.
+                       (mc-if (mc-and t1
+                                      (op-atom? r1)
+                                      (op-atom? r2))
+                              ;; Both atoms.
+                              (list
+                               (op-eq? r1 r2)
+                               (op-swap c r3))
+                              (mc-if (mc-and t1
+                                             (op-not t1)
+                                             (mc-not (op-atom? r2)))
+                                     ;; Both non-atoms.
+                                     (mc-spill (list t2 t3)
+                                               ;; Compute (car r1) & (car r2).
+                                               (mc-pop t1 r1)
+                                               (mc-pop t2 r2)
+                                               (mc-call ':fn-equal? r1 r2 r3)
+                                               (op-swap t1 r1)
+                                               (op-swap t2 r2)
+                                               (op-swap t3 r3) ;; Result of (equal? (cdr r1) (cdr r2)) is in t3.
+                                               (mc-call ':fn-equal? r1 r2 r3) ;; Result of (equal? (car r1) (car r2)) is in r3.
+                                               (op-and t3 r3)
+                                               (op-swap c r3) ;; Result of (and t3 r3) lands in r3.
+                                               ;; Restore arguments.
+                                               (op-swap t1 r1)
+                                               (op-swap t2 r2)
+                                               (mc-push r1 t1)
+                                               (mc-push r2 t2))
+                                     ;; An atom & non-atom.
+                                     (op-set r3 'nil))))))
 
 (define (fn-cons)
   (mc-define ':fn-cons ; (r1 r2) -> r3
